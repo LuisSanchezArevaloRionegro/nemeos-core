23969d543a70b207d15bcd5d7a40c99e2225adb4
Luis Sánchez-Arévalo Rionegro <luis.sanchezarevalo.developer@gmail.com>
2020-07-07 20:00:01 +0000

    Modify enviroment Variable, add Models and create function to init database if not exist

--- src/main.py
+++ src/main.py
@@ -9,12 +9,14 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
+from database import init_database
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+init_database()
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
